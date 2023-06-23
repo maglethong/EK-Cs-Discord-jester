dc759ceea32d451540b8afcb0fb509df1a53424f
Maglethong Spirr <maglethong@users.noreply.github.com>
2023-06-24 01:21:43 +0200

    Added dockerfile and docker start utility

---
+++ BuildAndRunDocker.ps1
@@ -0,0 +1,31 @@
+﻿docker build -t maglethong/ek/discord/jester:local .
+
+az login
+
+$key_vault_name = "dev-kv-ek-discord"
+
+$response = az keyvault secret show `
+    --name "Discord--Token" `
+    --vault-name $key_vault_name
+
+$Discord__Token =  ($response | ConvertFrom-Json).Value
+
+$response = az keyvault secret show `
+    --name "Notion--Token" `
+    --vault-name $key_vault_name
+
+$Notion__Token =  ($response | ConvertFrom-Json).Value
+
+docker run `
+    --rm `
+    -d `
+    --name ek-discord-jester `
+    -p 9316:9316 `
+    -p 5025:5025 `
+    -p 7144:7144 `
+    -p 44376:44376 `
+    -e Discord__Token=$Discord__Token `
+    -e Notion__Token=$Notion__Token `
+    -e ASPNETCORE_ENVIRONMENT='Development' `
+    -e DOTNET_ENVIRONMENT='Development' `
+    maglethong/ek/discord/jester:local
