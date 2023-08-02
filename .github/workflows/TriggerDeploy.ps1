404f4e9e16c0d2fd01b9e6cf9f4547bdc8ae2e66
Maglethong Spirr <maglethong@users.noreply.github.com>
2023-08-02 16:52:33 +0200

    wip

---
+++ .github/workflows/TriggerDeploy.ps1
@@ -0,0 +1,18 @@
+﻿$storageAccountName = $env:RELEASE_STORAGE_ACCOUNT_NAME
+$storageContainerName = $env:RELEASE_STORAGE_ACCOUNT_CONTAINER_NAME
+$resource_group_name = $env:RESOURCE_GROUP_NAME
+$vm_name = $env:DEPLOY_VM_NAME
+$key_vault_name = $env:KEY_VAULT_NAME
+$subscription = ((az account list | ConvertFrom-Json) | WHERE isDefault -eq $true).id
+$tenant = ((az account list | ConvertFrom-Json) | WHERE isDefault -eq $true).tenantId
+$deployStorageUrl="https://${storageAccountName}.blob.core.windows.net/${$storageContainerName}/"
+$targetFileName = "./DockerImage-${{ github.sha }}.tar"
+
+az vm run-command invoke `
+          --resource-group $resource_group_name `
+          --name $vm_name `
+          --command-id "SetupVm" `
+          --scripts "./.github/workflows/Deploy.sh >> Deploy.log" `
+          --parameters $deployStorageUrl `
+                       "${{ github.sha }}" `
+                       "${{ secrets.AZURE_CREDENTIALS }}"
---
+++ .github/workflows/UploadToAzureStorage.ps1
@@ -0,0 +1,39 @@
+﻿$imageFile = "./DockerImage.tar"
+$storageAccountName = $env:RELEASE_STORAGE_ACCOUNT_NAME
+$storageContainerName = $env:RELEASE_STORAGE_ACCOUNT_CONTAINER_NAME
+$targetFileName = "./DockerImage-${{ github.sha }}.tar"
+$subscription = ((az account list | ConvertFrom-Json) | WHERE isDefault -eq $true).id
+$tenant = ((az account list | ConvertFrom-Json) | WHERE isDefault -eq $true).tenantId
+
+Write-Host Uploading release to $targetFileName
+
+az storage blob upload `
+    --file $imageFile `
+    --name $targetFileName `
+    --account-name $storageAccountName `
+    --container-name $storageContainerName `
+    --auth-mode login `
+    --timeout 300
+
+$existingReleasesRaw = az storage blob list `
+    --account-name $storageAccountName `
+    --container-name $storageContainerName `
+    --auth-mode login `
+    --timeout 300
+
+$toDeleteList = ($existingReleasesRaw | `
+                 ConvertFrom-Json | `
+                 Sort-Object $_.properties.lastModified) | `
+    Select-Object `
+    -Skip 3 `
+    -Property name
+
+foreach ($fileToDelete in $toDeleteList) {
+    Write-Host Deleting old release file: $fileToDelete.Name
+    az storage blob delete `
+        --name $fileToDelete.Name `
+        --account-name $storageAccountName `
+        --container-name $storageContainerName `
+        --auth-mode login `
+        --timeout 300
+}
